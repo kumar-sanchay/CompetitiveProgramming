e5ebf828a0b0e97220ab78a2fb820a51676a0891
kumar-sanchay <dsanchaykumar@gmail.com>
2021-06-08 00:24:37 +0530

    min cost stair problem ans

--- min_cost_climbing_stairs.java
+++ min_cost_climbing_stairs.java
@@ -1,9 +1,33 @@
+import java.util.*;
 
-
-class Solution{
-
-    public static void main(String args[]){
-
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int cache[] = new int[cost.length];
+        Arrays.fill(cache, -1);
         
+        int a = f(cost, 0, cache);
+        int b = f(cost, 1, cache);
+        
+        
+        System.out.println(b);
+        if(a<b)
+            return a;
+        else 
+            return b;
+    }
+
+    public static int f(int cost[], int idx, int cache[]){
+        
+        if(cache[idx]!=-1)
+            return cache[idx];
+        
+        int n = cost.length -1;
+        if(idx>=n-1)
+            return cost[idx];
+
+        int res = cost[idx] + Math.min(f(cost, idx+1, cache), f(cost, idx+2, cache));
+        
+        cache[idx] = res;
+        return res;
     }
 }
